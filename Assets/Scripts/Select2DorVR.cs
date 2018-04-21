1a256fa95900f29f9212a5b41a7016454ed37331
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2018-04-21 11:55:34 +0530

    initial commit and added the initial scenes

---
+++ Assets/Scripts/Select2DorVR.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.SceneManagement;
+public class Select2DorVR : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	IEnumerator SwitchToVR() {
+		string desiredDevice = "cardboard";
+		XRSettings.LoadDeviceByName (desiredDevice);
+		StartCoroutine (waitforframe ());
+		yield return 0;
+	}
+	IEnumerator waitforframe () {
+		yield return 0;
+		XRSettings.enabled = true;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (XRSettings.loadedDeviceName.Equals("cardboard")) {
+			SceneManager.LoadSceneAsync ("3DPuzzleScene");
+		}
+	}
+	public void changeSelected(string selection) {
+		if (selection.Equals ("2D")) {
+			SceneManager.LoadSceneAsync ("2DPuzzleScene");
+		} else {
+			StartCoroutine (SwitchToVR());
+		}
+	}
+}
