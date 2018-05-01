543d553c65c0436730c13e90378cc4c69d104232
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2018-05-01 14:54:52 +0530

    Added puzzle panel instantiator script that instantiates panel according to texture width and height

---
+++ Assets/Scripts/PuzzlePanelInstantiator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePanelInstantiator : MonoBehaviour {
+	public GameObject[] PuzzlePanelPrefabs;
+	public GameObject PuzzleCanvas;
+	// Use this for initialization
+	void Start () {
+		GameObject pp;
+		if (SaveData.control.cubeTex.width > SaveData.control.cubeTex.height) {
+			pp = Object.Instantiate (PuzzlePanelPrefabs [0], PuzzleCanvas.transform, false);
+		} else if (SaveData.control.cubeTex.width < SaveData.control.cubeTex.height) {
+			pp = Object.Instantiate (PuzzlePanelPrefabs [1], PuzzleCanvas.transform, false);
+		} else {
+			pp = Object.Instantiate (PuzzlePanelPrefabs [2], PuzzleCanvas.transform, false);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
