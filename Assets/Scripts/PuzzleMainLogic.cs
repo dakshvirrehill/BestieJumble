a196c032a177f3649b75ca4c2283ddf5b1963a5a
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2018-05-02 14:57:20 +0530

    shuffled the puzzle cubes using fisher yates shuffle algorithm

--- Assets/Scripts/PuzzleMainLogic.cs
+++ Assets/Scripts/PuzzleMainLogic.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -22,25 +23,65 @@ public class PuzzleMainLogic : MonoBehaviour {
 		PuzzleCubes= new GameObject[n,m];
 		PuzzleCubePosition = new Vector2[n,m];
 		int count = 0;
+		//Debug.Log ("Starting PuzzleCubePositions");
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				PuzzleCubes [i,j] = gameObject.transform.GetChild (count).gameObject;
 				count++;
 				PuzzleCubes [i, j].GetComponent<RawImage> ().texture = SaveData.control.cubeTex;
 				PuzzleCubes [i, j].GetComponent<PuzzleCube2D> ().actualPos = new Vector2 (i, j);
+				PuzzleCubes [i, j].GetComponent<PuzzleCube2D> ().currentPos = new Vector2 (i, j);
 				PuzzleCubePosition [i,j] = new Vector2(PuzzleCubes [i,j].GetComponent<RectTransform> ().localPosition.x,PuzzleCubes [i,j].GetComponent<RectTransform> ().localPosition.y);
-				//Debug.Log (PuzzleCubePosition [i, j]);
+			//	Debug.Log ("Value for i=" + i + " and j=" + j);
+			//	Debug.Log (PuzzleCubePosition [i, j]);
 			}
 		}
-		StartCoroutine (JumblePuzzle());
+		JumblePuzzle();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-	IEnumerator JumblePuzzle() {
-		
-		yield return new WaitForSeconds (5.0f);
+	void JumblePuzzle() {
+		System.Random r = new System.Random ();
+		//Debug.Log ("Values in jumblepuzzle function");
+		int row=PuzzleCubes.GetLength (0);
+		int column = PuzzleCubes.GetLength (1);
+		for (int k = 0; k < column; k++) {
+			for (int i = row - 1; i >= 0; i--) {
+				for (int j = column - 1; j >= 0; j--) {
+					int n = r.Next (0, (i + 1));
+					int m = r.Next (0, (j + 1));
+					Vector2 currentpos = PuzzleCubes [i, j].GetComponent<PuzzleCube2D> ().currentPos;
+					Vector2 actualpos = PuzzleCubes [n, m].GetComponent<PuzzleCube2D> ().actualPos;
+					if (actualpos.x == currentpos.x) {
+						n = (n + 1) % row;
+					}
+					if (actualpos.y == currentpos.y) {
+						m = (m + 1) % column;
+					}
+					Vector2 actualposij = PuzzleCubes [i, j].GetComponent<PuzzleCube2D> ().actualPos;
+					Vector2 currentposmn = PuzzleCubes [n, m].GetComponent<PuzzleCube2D> ().currentPos;
+					if (actualposij.x == currentposmn.x) {
+						n = (n + 1) % row;
+					}
+					if (actualposij.x == currentposmn.x) {
+						m = (m + 1) % column;
+					}
+					PuzzleCubes [i, j].GetComponent<PuzzleCube2D> ().currentPos = PuzzleCubes [n, m].GetComponent<PuzzleCube2D> ().currentPos;
+					PuzzleCubes [n, m].GetComponent<PuzzleCube2D> ().currentPos = currentpos;
+					PuzzleCubes [i, j].GetComponent<RectTransform> ().localPosition = PuzzleCubePosition [n, m];
+					PuzzleCubes [n, m].GetComponent<RectTransform> ().localPosition = PuzzleCubePosition [i, j];
+					PuzzleCubePosition [i, j] = PuzzleCubes [i, j].GetComponent<RectTransform> ().localPosition;
+					PuzzleCubePosition [n, m] = PuzzleCubes [n, m].GetComponent<RectTransform> ().localPosition;
+					//Debug.Log ("i=" + i + " j=" + j + " n=" + n + " m=" + m);
+					//Debug.Log ("CurrentPos of ij= " + PuzzleCubes [i, j].GetComponent<PuzzleCube2D> ().currentPos);
+					//Debug.Log ("CurrentPos of nm= " + PuzzleCubes [n, m].GetComponent<PuzzleCube2D> ().currentPos);
+					//Debug.Log ("PuzzleCubePosition of ij  " + PuzzleCubePosition [i, j]);
+					//Debug.Log ("PuzzleCubePosition of nm" + PuzzleCubePosition [n, m]);
+				}
+			}
+		}
 	}
 }
