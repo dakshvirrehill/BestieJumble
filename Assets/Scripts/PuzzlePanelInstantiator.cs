a1f6c5fcf6161cd387eea5c9c196906006205751
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2018-05-03 15:06:24 +0530

    removed redundant assignment

--- Assets/Scripts/PuzzlePanelInstantiator.cs
+++ Assets/Scripts/PuzzlePanelInstantiator.cs
@@ -7,13 +7,12 @@ public class PuzzlePanelInstantiator : MonoBehaviour {
 	public GameObject PuzzleCanvas;
 	// Use this for initialization
 	void Start () {
-		GameObject pp;
 		if (SaveData.control.cubeTex.width > SaveData.control.cubeTex.height) {
-			pp = Object.Instantiate (PuzzlePanelPrefabs [0], PuzzleCanvas.transform, false);
+			Object.Instantiate (PuzzlePanelPrefabs [0], PuzzleCanvas.transform, false);
 		} else if (SaveData.control.cubeTex.width < SaveData.control.cubeTex.height) {
-			pp = Object.Instantiate (PuzzlePanelPrefabs [1], PuzzleCanvas.transform, false);
+			Object.Instantiate (PuzzlePanelPrefabs [1], PuzzleCanvas.transform, false);
 		} else {
-			pp = Object.Instantiate (PuzzlePanelPrefabs [2], PuzzleCanvas.transform, false);
+			Object.Instantiate (PuzzlePanelPrefabs [2], PuzzleCanvas.transform, false);
 		}
 	}
 	// Update is called once per frame
