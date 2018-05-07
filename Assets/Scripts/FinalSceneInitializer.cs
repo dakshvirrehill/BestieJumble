bda45ab9b617e3fb46ac6de2221b6222a8c770a9
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2018-05-07 13:29:43 +0530

    added initializer code according to XR device name

---
+++ Assets/Scripts/FinalSceneInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+public class FinalSceneInitializer : MonoBehaviour {
+	public GameObject[] WinningScenePrefabs;
+	private GameObject ToBeDestroyed;
+	// Use this for initialization
+	void Start () {
+		if (XRSettings.loadedDeviceName.Equals ("cardboard")) {
+			ToBeDestroyed = Object.Instantiate (WinningScenePrefabs [0], new Vector3 (0f, 0f, 0f), new Quaternion (0f, 0f, 0f, 0f));
+		} else {
+			ToBeDestroyed = Object.Instantiate (WinningScenePrefabs [1], new Vector3 (0f, 0f, 0f), new Quaternion (0f, 0f, 0f, 0f));
+		}
+		ToBeDestroyed.transform.DetachChildren ();
+		Destroy (ToBeDestroyed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
