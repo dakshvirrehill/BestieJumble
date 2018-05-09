f8bcacc1a718247671555d4359568a7046d737f4
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2018-05-09 17:28:52 +0530

    added deactivate and activate button methods and updated screenshot to be valid in android as well

--- Assets/Scripts/TwoDFinalLogic.cs
+++ Assets/Scripts/TwoDFinalLogic.cs
@@ -6,12 +6,20 @@ using UnityEngine.SceneManagement;
 using TMPro;
 public class TwoDFinalLogic : MonoBehaviour {
 	public GameObject winningPanel;
+	private GameObject BackButton;
+	private GameObject PrintButton;
 	// Use this for initialization
 	void Start () {
+		BackButton = winningPanel.transform.GetChild (6).gameObject;
+		PrintButton = winningPanel.transform.GetChild (7).gameObject;
 		winningPanel.transform.GetChild (2).gameObject.GetComponent<RawImage> ().texture = SaveData.control.cubeTex;
 		winningPanel.transform.GetChild (3).gameObject.GetComponent<TextMeshProUGUI> ().text = SaveData.control.username;
-		winningPanel.transform.GetChild (6).gameObject.GetComponent<Button> ().onClick.AddListener (() => BackToMain ());
-		winningPanel.transform.GetChild (7).gameObject.GetComponent<Button> ().onClick.AddListener (() => Printed ());
+		winningPanel.transform.GetChild (5).gameObject.GetComponent<TMP_InputField> ().onSelect.AddListener ((string arg0) => DeactivateButtons (arg0));
+		winningPanel.transform.GetChild (5).gameObject.GetComponent<TMP_InputField> ().onDeselect.AddListener ((string arg0) => ActivateButtons (arg0));
+		winningPanel.transform.GetChild (5).gameObject.GetComponent<TMP_InputField> ().onEndEdit.AddListener ((string arg0) => ActivateButtons (arg0));
+		BackButton.GetComponent<Button> ().onClick.AddListener (() => BackToMain ());
+		PrintButton.GetComponent<Button> ().onClick.AddListener (() => Printed ());
+		DeactivateButtons ("default");
 		winningPanel.transform.GetChild (8).gameObject.GetComponent<Button> ().onClick.AddListener (() => new MainSceenLogic ().QuitGame ());
 	}
 	
@@ -27,10 +35,25 @@ public class TwoDFinalLogic : MonoBehaviour {
 		Destroy (tex);
 		tex1.SetPixels (cols);
 		tex1.Apply ();
-		System.IO.File.WriteAllBytes(Application.persistentDataPath + "/SavedScreen.png", tex1.EncodeToPNG());
+		string path = Application.persistentDataPath + "/" + SaveData.control.username + ".png";
+		if (Application.platform == RuntimePlatform.Android) {
+			AndroidJavaClass environment = new AndroidJavaClass ("android.os.Environment");
+			AndroidJavaObject filepath = environment.CallStatic<AndroidJavaObject> ("getExternalStoragePublicDirectory", environment.GetStatic<string> ("DIRECTORY_PICTURES"));
+			filepath.Call<bool> ("mkdirs");
+			path = filepath.Call<string> ("getAbsolutePath") + "/" + SaveData.control.username + ".png";
+		}
+		System.IO.File.WriteAllBytes (path, tex1.EncodeToPNG());
 		Destroy (tex1);
 	}
 	public void BackToMain() {
 		SceneManager.LoadSceneAsync ("MainScene");
+	}
+	public void ActivateButtons(string defaulte) {
+		BackButton.SetActive(true);
+		PrintButton.SetActive(true);
+	}
+	public void DeactivateButtons(string defaulte) {
+		BackButton.SetActive(false);
+		PrintButton.SetActive(false);
 	}
 }
