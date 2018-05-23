bdf5a29bd0b883637a04058187786a72b8f3870f
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2018-05-23 16:01:58 +0530

    added required info for selector and non grid positons

---
+++ Assets/Scripts/NonGridIsUsed.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonGridIsUsed : MonoBehaviour {
+	public bool isUsed;
+	// Use this for initialization
+	void Start () {
+		isUsed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
