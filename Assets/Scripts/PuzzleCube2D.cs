7abdf0bca95f0f80fce8f76becd9989cc357c3f0
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2018-05-23 15:58:31 +0530

    added the index of the non grid value for vr cubes

--- Assets/Scripts/PuzzleCube2D.cs
+++ Assets/Scripts/PuzzleCube2D.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PuzzleCube2D : MonoBehaviour {
 	public Vector2 currentPos;
 	public Vector2 actualPos;
+	public int ngv;
 	public bool isCorrect;
 	// Use this for initialization
 	void Start () {
