2372b861b86b660308d324310e8fde0edb3b7c6c
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2018-06-13 13:06:33 +0530

    added opening of treasure chest code and corrected sewer opening code as well

--- Assets/Scripts/OpenSewer.cs
+++ Assets/Scripts/OpenSewer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OpenSewer : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
 		
@@ -24,7 +23,7 @@ public class OpenSewer : MonoBehaviour {
 			}
 		}
 		if (found) {
-			iTween.MoveTo (gameObject, iTween.Hash ("position", new Vector3 (-12.62f, 0f, 6.93f), "time", 3f, "islocal", true, "oncompletetarget", gameObject, "oncomplete", "BringCubeUp", "oncompleteparams", hitColliders [i].gameObject));
+			iTween.MoveTo (gameObject, iTween.Hash ("position", new Vector3(12.62f,0f,6.93f), "time", 3f, "islocal", true, "oncompletetarget", gameObject, "oncomplete", "BringCubeUp", "oncompleteparams", hitColliders [i].gameObject));
 		} else {
 			
 		}
@@ -33,6 +32,6 @@ public class OpenSewer : MonoBehaviour {
 		iTween.MoveTo(PuzzleCube,iTween.Hash("position",PuzzleCube.transform.position+new Vector3(0f,1.28f,0f),"time",3f,"oncompletetarget", gameObject, "oncomplete", "CloseSewer"));
 	}
 	public void CloseSewer() {
-		iTween.MoveTo (gameObject, iTween.Hash ("position", new Vector3 (-10.42f, 0f, 6.93f), "time", 3f, "islocal", true));
+		iTween.MoveTo (gameObject, iTween.Hash ("position", new Vector3 (10.42f,0f,6.93f), "time", 3f, "islocal", true));
 	}
 }
---
+++ Assets/Scripts/OpenTheCrate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenTheCrate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OpenCrate() {
+		Collider[] hitColliders = Physics.OverlapSphere (gameObject.transform.position, 0.65f);
+		bool found = false;
+		int i = 0;
+		for (i = 0; i < hitColliders.Length; i++) {
+			if (hitColliders [i].gameObject.tag == "PuzzleCube") {
+				found = true;
+				break;
+			}
+		}
+		if (found) {
+			iTween.RotateTo (gameObject, iTween.Hash ("rotation", new Vector3(90f,0f,0f), "time", 3f, "oncompletetarget", gameObject, "oncomplete", "BringCubeUp", "oncompleteparams", hitColliders [i].gameObject));
+		} else {
+
+		}
+	}
+	void BringCubeUp(GameObject PuzzleCube) {
+		iTween.MoveTo(PuzzleCube,iTween.Hash("position",PuzzleCube.transform.position+new Vector3(0f,1.28f,0f),"time",3f,"oncompletetarget", gameObject, "oncomplete", "CloseCrate"));
+	}
+	void CloseCrate() {
+		iTween.RotateTo (gameObject, new Vector3 (0f, 0f, 0f), 3f);
+	}
+}
