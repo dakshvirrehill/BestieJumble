df81ce6288b08ff2dc199c0e61cf8aa119a47527
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2018-06-14 16:35:19 +0530

    added code to change the distance of reticle so that cubes are not clickable from a long distance

---
+++ Assets/Scripts/MaxReticleDistanceChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxReticleDistanceChanger : MonoBehaviour {
+	private float angley;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.GetChild (0).rotation.eulerAngles.y > 180f) {
+			angley = Mathf.Abs (transform.GetChild (0).rotation.eulerAngles.y - 360f);
+		} else {
+			angley = transform.GetChild (0).rotation.eulerAngles.y;
+		}
+		if (transform.position.y > 19f && (angley >= 0f && angley <= 50f)) {
+			transform.GetChild (0).GetChild (0).gameObject.GetComponent<GvrReticlePointer> ().maxReticleDistance = 38f;
+		} else {
+			transform.GetChild (0).GetChild (0).gameObject.GetComponent<GvrReticlePointer> ().maxReticleDistance = 10f;
+		}
+	}
+}
