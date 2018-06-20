19bd2c595cdff9d91f16bd3fc2323947a5b06ddb
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2018-06-20 17:57:30 +0530

    refined boat movement and orientation of the boat as it moves and added waypoint on island

--- Assets/Scripts/MoveBoat.cs
+++ Assets/Scripts/MoveBoat.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class MoveBoat : MonoBehaviour {
 	private Vector3 boatPos;
 	private Vector3 islandPos;
+	private Vector3[] toMove;
+	private GameObject eventSystem;
 	// Use this for initialization
 	void Start () {
+		toMove = new Vector3[2];
+		toMove[0] = new Vector3(-36.87f,0.344f,-4.48f);
 		boatPos = gameObject.transform.position;
 		islandPos = new Vector3 (-77.3f, 0.344f, -8.55f);
+		eventSystem = GameObject.Find ("GvrEventSystem");
 	}
 	
 	// Update is called once per frame
@@ -16,19 +21,22 @@ public class MoveBoat : MonoBehaviour {
 		
 	}
 	public void MoveTheBoat(GameObject player) {
+		eventSystem.SetActive (false);
 		iTween.MoveTo (player, iTween.Hash ("position", gameObject.transform.position + new Vector3 (0f, 1f, 0f), "time", 3f, "oncomplete", "MoveForReal","oncompletetarget",gameObject,"oncompleteparams",player));
 	}
 	void MoveForReal(GameObject player) {
-		player.transform.SetParent (gameObject.transform);
-		Vector3 toMove;
 		if (gameObject.transform.position == boatPos) {
-			toMove = islandPos;
+			iTween.RotateTo (gameObject, new Vector3 (0f, 133f, 0f), 15f);
+			toMove[1] = islandPos;
 		} else {
-			toMove = boatPos;
+			iTween.RotateTo (gameObject, new Vector3 (0f, -90f, 0f), 15f);
+			toMove[1] = boatPos;
 		}
-		iTween.MoveTo (gameObject, iTween.Hash ("position", toMove, "time", 15f, "oncomplete", "RemoveChildren", "oncompletetarget", gameObject,"oncompleteparams",player,"orienttopath",true));
+		player.transform.SetParent (gameObject.transform);
+		iTween.MoveTo (gameObject, iTween.Hash ("path", toMove , "time", 15f, "oncomplete", "RemoveChildren", "oncompletetarget", gameObject,"oncompleteparams",player));
 	}
 	void RemoveChildren(GameObject player) {
 		player.transform.parent=null;
+		eventSystem.SetActive (true);
 	}
 }
