1fba91189be0636a0abd2043f9de090ae30b6dec
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2018-07-26 19:52:42 +0530

    added script for VRwinning logic

---
+++ Assets/Scripts/VRWinningLogic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class VRWinningLogic : MonoBehaviour {
+	public GameObject Cowboy;
+	public GameObject Player;
+	public GameObject Menu;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
