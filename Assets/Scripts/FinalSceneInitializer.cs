c72ebac24fefd1d7c0be1fe02a4439edb4cabef3
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2018-07-27 14:37:22 +0530

    added code in function instead of start function

--- Assets/Scripts/FinalSceneInitializer.cs
+++ Assets/Scripts/FinalSceneInitializer.cs
@@ -7,16 +7,18 @@ public class FinalSceneInitializer : MonoBehaviour {
 	private GameObject ToBeDestroyed;
 	// Use this for initialization
 	void Start () {
+		SceneStarter ();
+	}
+	public void SceneStarter() {
 		if (XRSettings.loadedDeviceName.Equals ("cardboard")) {
 			ToBeDestroyed = Object.Instantiate (WinningScenePrefabs [0], new Vector3 (0f, 0f, 0f), new Quaternion (0f, 0f, 0f, 0f));
 		} else {
 			ToBeDestroyed = Object.Instantiate (WinningScenePrefabs [1], new Vector3 (0f, 0f, 0f), new Quaternion (0f, 0f, 0f, 0f));
+			ToBeDestroyed.transform.DetachChildren ();
+			Destroy (ToBeDestroyed);
 			//ToBeDestroyed.transform.GetChild(6).gameObject.GetComponent<TwoDFinalLogic>().winningPanel=ToBeDestroyed.transform.GetChild (1).GetChild (1);
 		}
-		ToBeDestroyed.transform.DetachChildren ();
-		Destroy (ToBeDestroyed);
 	}
-	
 	// Update is called once per frame
 	void Update () {
 		
