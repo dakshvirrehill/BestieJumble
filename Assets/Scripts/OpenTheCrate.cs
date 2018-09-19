d66e124259c7b8770054591258beb6a7f27e045c
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2018-09-19 17:24:53 +0530

    fixed problem of crate opening in wrong direction

--- Assets/Scripts/OpenTheCrate.cs
+++ Assets/Scripts/OpenTheCrate.cs
@@ -24,15 +24,15 @@ public class OpenTheCrate : MonoBehaviour {
 			}
 		}
 		if (found) {
-			iTween.RotateTo (transform.GetChild(1).gameObject, iTween.Hash ("rotation", new Vector3(90f,0f,0f), "time", 3f, "oncompletetarget", gameObject, "oncomplete", "BringCubeUp", "oncompleteparams", hitColliders [i].gameObject));
+			iTween.RotateTo (gameObject, iTween.Hash ("rotation", new Vector3(-90f,0f,0f), "time", 3f, "oncompletetarget", gameObject, "oncomplete", "BringCubeUp", "oncompleteparams", hitColliders [i].gameObject));
 		} else {
 
 		}
 	}
 	void BringCubeUp(GameObject PuzzleCube) {
-		iTween.MoveTo(PuzzleCube,iTween.Hash("position",PuzzleCube.transform.position+new Vector3(0f,1.28f,0f),"time",3f,"oncompletetarget", gameObject, "oncomplete", "CloseCrate"));
+		iTween.MoveTo(PuzzleCube,iTween.Hash("position",PuzzleCube.transform.position+new Vector3(0f,2.5f,0f),"time",3f,"oncompletetarget", gameObject, "oncomplete", "CloseCrate"));
 	}
 	void CloseCrate() {
-		iTween.RotateTo (transform.GetChild(1).gameObject, new Vector3 (0f, 0f, 0f), 3f);
+		iTween.RotateTo (gameObject, new Vector3 (0f, 0f, 0f), 3f);
 	}
 }
